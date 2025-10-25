152875af00e9a582849411385063f4a42f7e5795
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-10-25 16:51:18 +0000

    test: update.

--- src/test/data-constructor.spec.ts
+++ src/test/data-constructor.spec.ts
@@ -1,11 +1,13 @@
-// class TestData implements DataShape<number> {
-//   get value(): number { return 33; }
-//   constructor(value: number) {}
-//   public clear(): this { return this; }
-//   public destroy(): this { return this; }
-//   public lock(): this { return this; }s
-//   public set(): this { return this; }
-// }
+import { DataConstructor } from "@typedly/constructor";
+import { DataShape } from "../lib";
 
-// export const a: DataConstructor<string, DataShape<any>, any[]> = TestData;
+class TestData implements DataShape<number> {
+  get value(): number { return this.initialValue; }
+  constructor(private initialValue: number) {}
+  public clear(): this { return this; }
+  public destroy(): this { return this; }
+  public lock(): this { return this; }
+  public set(): this { return this; }
+}
 
+export const ctor: DataConstructor<number, DataShape<number>, DataShape<number>, any[]> = TestData;
