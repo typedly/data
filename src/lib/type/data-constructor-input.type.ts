d68a033bbada772eb0bbe3aae64358ead14fe83a
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-10-25 16:51:09 +0000

    refactor(`DataConstructorInput`): change `DataType` to `Instance` and update docs.

--- src/lib/type/data-constructor-input.type.ts
+++ src/lib/type/data-constructor-input.type.ts
@@ -3,10 +3,10 @@ import { DataConstructor, DataShape } from "../interface";
 /**
  * @description The input type for data constructors, with arguments support.
  * @export
- * @template Value 
- * @template {DataCore<Value>} DataType 
+ * @template Value The value type.
+ * @template {DataShape<Value>} Instance The data instance type.
  */
-export type DataConstructorInput<Value, DataType extends DataShape<Value>> =
-  | [DataConstructor<Value, DataType>, ...any[]]
-  | DataConstructor<Value, DataType>
+export type DataConstructorInput<Value, Instance extends DataShape<Value>> =
+  | [DataConstructor<Value, Instance>, ...any[]]
+  | DataConstructor<Value, Instance>
 ;
