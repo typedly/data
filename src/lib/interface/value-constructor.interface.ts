1c2bf021eefe072ba70181d266ff9add6d841511
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-10-26 22:33:02 +0000

    refactor(`ValueConstructor`): use `DataConstructor`.

--- src/lib/interface/value-constructor.interface.ts
+++ src/lib/interface/value-constructor.interface.ts
@@ -1,12 +1,17 @@
 // Interface.
+import { DataConstructor as BaseDataConstructor } from '@typedly/constructor';
 import { ValueShape } from "./value-shape.interface";
 /**
  * @description The constructor for value in data for advanced customization.
  * @export
  * @interface ValueConstructor
- * @template Value The type of value.
- * @template {ValueShape<Value>} Instance The type of value instance.
+ * @template Value 
+ * @template {ValueShape<Value>} Instance The instance.
+ * @template {readonly any[]} [Args=any[]] 
+ * @extends {BaseDataConstructor<Value, ValueShape<Value>, Instance, [...Args]>}
  */
-export interface ValueConstructor<Value, Instance extends ValueShape<Value>> {
-  new (value: Value, ...args: any[]): Instance;
-}
+export interface ValueConstructor<
+  Value,
+  Instance extends ValueShape<Value>,
+  Args extends readonly any[] = any[]
+> extends BaseDataConstructor<Value, ValueShape<Value>, Instance, [...Args]> {}
