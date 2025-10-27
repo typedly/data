ae14df3fb89f84d6769a1cdba12bc184d1174bc4
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-10-27 10:30:56 +0000

    chore: remove cause it is unnecessary.

--- src/lib/type/index.ts
+++ src/lib/type/index.ts
@@ -1,2 +1,2 @@
 export type { DataConstructorInput } from './data-constructor-input.type';
-export type { ValueConstructorInput } from './value-constructor-input.type';
+export type { DataConstructorTuple } from './data-constructor-tuple.type';
