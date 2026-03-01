85b9bf5c5c7664130992115808a4d78c54569116
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-03-01 20:14:44 +0000

    refactor(`DataAdapter`): update interface to include configuration type for improved type safety

--- src/lib/interface/data-adapter.interface.ts
+++ src/lib/interface/data-adapter.interface.ts
@@ -1,14 +1,18 @@
 // Interface.
-import { DataShape } from './data-shape.interface';
+import { DataShape } from './data.shape';
 /**
  * @description The adapter interface for data types.
  * @export
  * @interface DataAdapter
  * @template T The type of the data value.
+ * @template C The type of the configuration object.
  * @template {boolean} [A=false] The type of the return values for methods.
- * @extends {DataShape<T, A>}
+ * @extends {DataShape<T, C, A>}
  */
-export interface DataAdapter<T, A extends boolean = false>
-  extends DataShape<T, A> {
-    version?: string;
+export interface DataAdapter<
+  T,
+  C,
+  A extends boolean = C extends { async?: boolean } ? C['async'] extends boolean ? C['async'] : false : false
+> extends DataShape<T, C, A> {
+  version?: string;
 }
