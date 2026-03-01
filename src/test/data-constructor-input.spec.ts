6ed470b604a5ee7427be830da5ffab7353594cde
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-03-01 20:16:51 +0000

    test: enhance TestData and TestDataClass for improved type safety and structure

--- src/test/data-constructor-input.spec.ts
+++ src/test/data-constructor-input.spec.ts
@@ -1,34 +1,44 @@
-import { DataConstructorInput } from "../lib";
+import { DataAdapter, DataConstructorInput } from "../lib";
 import { DataShape } from "../lib";
 
-export class TestData<Type> implements DataShape<Type> {
-  get value(): Type { return 27 as Type; }
-  setValue(value: Type): this { return this; }
-  getValue(): Type {
-    return 27 as Type;
+export class TestData<
+  T,
+  C extends { async?: boolean },
+> implements DataShape<T, C, false> {
+  get async(): false { return false; }
+  get configuration(): C | undefined { return undefined; }
+  get value(): T { return 27 as T; }
+  setValue(value: T): this { return this; }
+  getValue(): T {
+    return 27 as T;
   }
   clear(): this { return this; }
   destroy(): this { return this; }
   lock(): this { return this; };
-  constructor(value: Type, ...args: any[]) {
+  constructor(settings: C, value: T, ...args: any[]) {
     console.log(`Instantiated Tuple`, value, ...args);
   }
 }
 
-export class TestDataClass<Value, DataType extends DataShape<Value>, Args extends any[]> {
-  constructor(value: Value, dataCtor: DataConstructorInput<Value, DataType, Args>);
-  constructor(value: Value, dataCtor: any) {
+export class TestDataClass<
+  I extends DataShape<T, C, R>,
+  A extends DataAdapter<T, C, R>,
+  C extends { async?: boolean },
+  T,
+  R extends boolean = C['async'] extends boolean ? C['async'] : false,
+  G extends readonly any[] = any[]
+> {
+  constructor(settings: C, value: T, dataCtor: DataConstructorInput<I, A, C, T, R, G>);
+  constructor(settings: C, value: T, dataCtor: any) {
     console.log(`DataConstructorInput`, value, dataCtor[1]);
     if (Array.isArray(dataCtor)) {
-      new dataCtor[0](value, ...dataCtor.slice(1));
+      new dataCtor[0](settings, value, ...dataCtor.slice(1));
     }
   }
 }
 
 // const testData: TestDataClass<number, TestData<number>, [number, number]>
-const testData = new TestDataClass(37, [TestData, 27, 127]);
+const testData = new TestDataClass({}, 37 as number, [TestData, 27, 127]);
 
 // const testData1: TestDataClass<number, DataShape<number>, any[]>
-const testData1 = new TestDataClass(37 as number, TestData);
-
-
+const testData1 = new TestDataClass({}, 37 as number, TestData);
