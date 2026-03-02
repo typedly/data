cf4607bd9419a15824f59dfada9e703565e0ab29
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-03-02 00:36:26 +0000

    refactor(`DataAdapterConstructor`): update type templates for improved type safety and inference

--- src/lib/constructor/lib/data-adapter.constructor.ts
+++ src/lib/constructor/lib/data-adapter.constructor.ts
@@ -1,20 +1,20 @@
 /// Interface
-import { DataAdapter } from "../../interface";
+import { DataAdapterShape } from "../../interface";
 /**
  * @description The constructor interface for data adapters.
  * @export
  * @interface DataAdapterConstructor
- * @template {DataAdapter<T, C, R>} I The data adapter instance type.
- * @template {{ async?: boolean }} C The configuration type.
- * @template T The underlying value type.
- * @template {boolean} [R=C['async'] extends boolean ? C['async'] : false] The async flag.
+ * @template {DataAdapterShape<T, C, R>} I The data adapter instance type.
+ * @template {{ async?: boolean }} [C=I extends DataAdapterShape<any, infer V, any> ? V : any] The configuration type, inferred from I if possible.
+ * @template [T=I extends DataAdapterShape<infer U, any, any> ? U : any] The underlying value type, inferred from I if possible.
+ * @template {boolean} [R=I extends DataAdapterShape<T, C, infer U> ? U extends boolean ? U : false : false] The async flag, inferred from I if possible.
  * @template {readonly any[]} [G=any[]] Additional arguments.
  */
 export interface DataAdapterConstructor<
-  I extends DataAdapter<T, C, R>,
-  C extends { async?: boolean },
-  T,
-  R extends boolean = C['async'] extends boolean ? C['async'] : false,
+  I extends DataAdapterShape<T, C, R>,
+  C extends { async?: boolean } = I extends DataAdapterShape<any, infer V, any> ? V : any,
+  T = I extends DataAdapterShape<infer U, any, any> ? U : any,
+  R extends boolean = I extends DataAdapterShape<T, C, infer U> ? U extends boolean ? U : false : false,
   G extends readonly any[] = any[]
 > {
   new (
