b1b0f4838f8dcf126c56770886e082dfac7e3d87
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-03-02 01:36:26 +0000

    refactor(`DataConstructorTuple`): remove DataAdapter type and simplify type parameters for clarity

--- src/lib/type/data-constructor-tuple.type.ts
+++ src/lib/type/data-constructor-tuple.type.ts
@@ -1,20 +1,18 @@
 // Interface.
-import { DataShape, DataConstructor, DataAdapter } from "../";
+import { DataShape, DataConstructor } from "../";
 /**
  * @description The type to provide data constructor with arguments.
  * @export
- * @template {DataShape<T, C, R>} I 
- * @template {DataAdapter<T, C, R>} A 
- * @template {{ async?: boolean }} C 
- * @template T 
- * @template {boolean} [R=C['async'] extends boolean ? C['async'] : false] 
- * @template {readonly any[]} [G=any[]] 
+ * @template {DataShape<T, C, R>} I The type of the data instance.
+ * @template {{ async?: boolean }} [C=I extends DataShape<any, infer V, any> ? V : any] The type of the configuration, inferred from I if possible.
+ * @template [T=I extends DataShape<infer U, any, any> ? U : any] The type of value instance, inferred from I if possible.
+ * @template {boolean} [R=I extends DataShape<T, C, infer U> ? U extends boolean ? U : false : false] The type of the readonly flag, inferred from I if possible.
+ * @template {readonly any[]} [G=any[]] The type of the arguments tuple.
  */
 export type DataConstructorTuple<
   I extends DataShape<T, C, R>,
-  A extends DataAdapter<T, C, R>,
-  C extends { async?: boolean },
-  T,
-  R extends boolean = C['async'] extends boolean ? C['async'] : false,
+  C extends { async?: boolean } = I extends DataShape<any, infer V, any> ? V : any,
+  T = I extends DataShape<infer U, any, any> ? U : any,
+  R extends boolean = I extends DataShape<T, C, infer U> ? U extends boolean ? U : false : false,
   G extends readonly any[] = any[]
-> = [DataConstructor<I, A, C, T, R, G>, ...G];
+> = [DataConstructor<I, C, T, R, G>, ...G];
