e60cbb24226bbe9bd89270e1763152052f43e077
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-03-07 17:24:17 +0000

    feat(`DataConfig`): add `DataConfig` for configuration

---
+++ src/lib/type/data.config.ts
@@ -0,0 +1,14 @@
+// Interface
+import { DataSettings } from "../interface/data.settings";
+// Type
+import { InferAsync } from ".";
+/**
+ * @description The type for the data configuration, which can be either a full configuration object of `C` or just an async flag.
+ * @export
+ * @template {DataSettings<R> | undefined} C The type of the settings for configuration.
+ * @template {boolean} [R=InferAsync<C>] The type of the async flag.
+ */
+export type DataConfig<
+  C extends DataSettings<R> | undefined,
+  R extends boolean = InferAsync<C>
+> = C extends DataSettings<R> ? Required<C> : { async: R };
