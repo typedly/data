124314b781180e6c0e3952689cbc789aaa94cc35
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-03-10 01:48:15 +0000

    refactor(`AdaptableDataShape`): update import statements to use type imports and enhance type definition, use `Adaptable` trait

--- src/adaptable/lib/adaptable.data.shape.ts
+++ src/adaptable/lib/adaptable.data.shape.ts
@@ -1,24 +1,22 @@
 // Interface.
-import { DataAdapterShape } from "../../adapter";
-import { ConfigurableDataShape } from "../../configurable";
-import { DataSettings } from "../../lib/interface";
+import type { DataAdapterShape } from "../../adapter";
+import type { DataSettings, DataShape } from "../../lib/interface";
 // Type.
-import { InferAsyncOf, InferSettings, InferValue } from "../../lib/type";
+import type { InferAsyncOf, InferSettings, InferValue } from "../../lib/type";
+import type { Adaptable } from "../../traits";
 /**
  * @description The shape of a data type with optional adapter.
  * @export
  * @interface AdaptableDataShape
  * @template {DataAdapterShape<C, T, R> | undefined} [A=undefined] The adapter type.
- * @template {DataSettings<R> | undefined} [C=] The settings type, inferred from A if possible.
+ * @template {DataSettings<R> | undefined} [C=InferSettings<A>] The settings type, inferred from A if possible.
  * @template [T=InferValue<A>] The value type, inferred from A if possible.
- * @template {boolean} [R=C extends DataSettings<infer U> ? U extends boolean ? U : false : A extends DataAdapterShape<C, T, infer U> ? U extends boolean ? U : false : false] The async flag, inferred from C or A if possible.
- * @extends {ConfigurableDataShape<C, T, R>}
+ * @template {boolean} [R=InferAsyncOf<[C, A]>] The async flag, inferred from C or A if possible.
+ * @extends {Adaptable<A, C, T, R>}
  */
 export interface AdaptableDataShape<
   A extends DataAdapterShape<C, T, R> | undefined = undefined,
   C extends DataSettings<R> | undefined = InferSettings<A>,
   T = InferValue<A>,
   R extends boolean = InferAsyncOf<[C, A]>
-> extends ConfigurableDataShape<C, T, R> {
-  readonly adapter?: A;
-}
+> extends DataShape<T, R>, Adaptable<A, C, T, R> {}
