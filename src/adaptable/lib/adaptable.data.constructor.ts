59426d9d6387b76d94993c1dd008dc5017ef076a
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-03-10 01:47:30 +0000

    refactor(`AdaptableDataConstructor`): streamline import statements and update default type for additional arguments

--- src/adaptable/lib/adaptable.data.constructor.ts
+++ src/adaptable/lib/adaptable.data.constructor.ts
@@ -1,8 +1,13 @@
 // Interface.
 import { AdaptableDataShape } from './adaptable.data.shape';
-import { DataAdapterConstructor } from '../../adapter/lib/data-adapter.constructor';
-import { DataAdapterShape } from '../../adapter';
-import { DataSettings, InferAsyncOf, InferSettings, InferValue } from '../../lib';
+import { DataAdapterConstructor, DataAdapterShape } from '../../adapter';
+import {
+  DataSettings,
+  // Type.
+  InferAsyncOf,
+  InferSettings,
+  InferValue
+} from '../../lib';
 /**
  * @description The constructor interface for data types with adapter.
  * @export
@@ -12,7 +17,7 @@ import { DataSettings, InferAsyncOf, InferSettings, InferValue } from '../../lib
  * @template {{ async?: boolean }} [C=A extends DataAdapterShape<any, infer V, any> ? V : any] The configuration type, inferred from A if possible.
  * @template [T=A extends DataAdapterShape<infer U, any, any> ? U : any] The underlying value type, inferred from A if possible.
  * @template {boolean} [R=A extends DataAdapterShape<T, C, infer U> ? U extends boolean ? U : false : false] The async flag, inferred from A if possible.
- * @template {readonly any[]} [G=any[]] The additional arguments type.
+ * @template {readonly any[]} [G=[]] The additional arguments type.
  */
 export interface AdaptableDataConstructor<
   A extends DataAdapterShape<C, T, R> | undefined,
@@ -20,11 +25,11 @@ export interface AdaptableDataConstructor<
   C extends DataSettings<R> = InferSettings<A>,
   T = InferValue<A>,
   R extends boolean = InferAsyncOf<[C, A, I]>,
-  G extends readonly any[] = any[]
+  G extends readonly any[] = []
 > {
   new (
     settings: C,
-    value: T,
+    value: T | undefined,
     adapter: DataAdapterConstructor<A, C, T, R, G>,
     ...args: G
   ): I;
