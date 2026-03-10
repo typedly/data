7b46757ccd71bc31c58bd9595361d030637c66e3
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-03-10 01:50:12 +0000

    docs(`InferAsyncOf`): enhance documentation clarity by providing detailed descriptions for the async inference type

--- src/lib/type/infer-async-of.type.ts
+++ src/lib/type/infer-async-of.type.ts
@@ -1,10 +1,10 @@
 // Type.
 import { InferAsync } from "./infer-async.type";
 /**
- * @description
+ * @description Infers the async flag from a tuple of arguments, returning true if any of the arguments has an async flag set to true.
  * @export
- * @template {readonly any[]} A 
- * @template [F=false] 
+ * @template {readonly any[]} A The tuple of arguments to infer the async flag from.
+ * @template [F=false] The default async flag value if the tuple is empty.
  */
 export type InferAsyncOf<A extends readonly any[], F = false> =
   A extends [infer Head, ...infer Tail]
