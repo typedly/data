7dee3eb5ffeebbae0b97bdd506d0eb5e8a8e4e39
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-03-10 01:47:11 +0000

    refactor(`DataConstructor`): update import statements to use type imports and enhance documentation clarity

--- src/lib/interface/data.constructor.ts
+++ src/lib/interface/data.constructor.ts
@@ -1,24 +1,26 @@
 // Interface.
-import { DataShape } from './data.shape';
+import type { DataShape } from './data.shape';
 // Type.
-import { InferAsync, InferValue } from '../type';
+import type { InferAsync, InferValue } from '../type';
 /**
  * @description The constructor interface for data types.
  * @export
  * @interface DataConstructor
  * @template {DataShape<T, R>} I The data instance type.
- * @template [T=InferValue<I>] The data type.
+ * @template [T=InferValue<I>] The data type inferred from `I`.
  * @template {boolean} [R=InferAsync<I>] The async flag, inferred from `I` if possible.
- * @template {readonly any[]} [G=any[]] Additional arguments.
+ * @template {readonly any[]} [G=[]] Additional arguments.
  */
 export interface DataConstructor<
   I extends DataShape<T, R>,
   T = InferValue<I>,
   R extends boolean = InferAsync<I>,
-  G extends readonly any[] = any[]
+  G extends readonly any[] = []
 > {
   new (
+    /** The optional initial value for the data instance. */
     value: T,
+    /** Additional arguments for the constructor. */
     ...args: G
   ): I;
 }
