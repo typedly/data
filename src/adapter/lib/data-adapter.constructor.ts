6f0581a06c37bac4ec0b1eabeab743d21df9d120
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-03-10 01:50:01 +0000

    refactor(`DataAdapterConstructor`): update import statements to use type imports and enhance type definitions for improved clarity

--- src/adapter/lib/data-adapter.constructor.ts
+++ src/adapter/lib/data-adapter.constructor.ts
@@ -1,28 +1,28 @@
 /// Interface
-import { DataAdapterShape } from "./data-adapter.shape";
-import { DataSettings, InferAsync, InferValue } from "../../lib";
+import type { DataAdapterShape } from "./data-adapter.shape";
+import type { DataSettings, InferAsync, InferValue } from "../../lib";
 // Type.
-import { InferSettings } from "../../lib/type";
+import type { InferSettings } from "../../lib/type";
 /**
  * @description The constructor interface for data adapters.
  * @export
  * @interface DataAdapterConstructor
- * @template {DataAdapterShape<C, T, R>} I The data adapter instance type.
- * @template {{ async?: boolean }} [C=I extends DataAdapterShape<any, infer V, any> ? V : any] The configuration type, inferred from I if possible.
- * @template [T=I extends DataAdapterShape<infer U, any, any> ? U : any] The underlying value type, inferred from I if possible.
- * @template {boolean} [R=I extends DataAdapterShape<T, C, infer U> ? U extends boolean ? U : false : false] The async flag, inferred from I if possible.
- * @template {readonly any[]} [G=any[]] Additional arguments.
+ * @template {DataAdapterShape<C, T, R> | undefined} I The adapter instance type, which can be undefined for more flexible inference.
+ * @template {DataSettings<R>} [C=InferSettings<I>] The configuration type, inferred from I if possible.
+ * @template [T=InferValue<I>] The underlying value type, inferred from I if possible.
+ * @template {boolean} [R=InferAsync<I>] The async flag, inferred from I if possible.
+ * @template {readonly any[]} [G=[]] Additional arguments.
  */
 export interface DataAdapterConstructor<
   I extends DataAdapterShape<C, T, R> | undefined,
   C extends DataSettings<R> = InferSettings<I>,
   T = InferValue<I>,
   R extends boolean = InferAsync<I>,
-  G extends readonly any[] = any[]
+  G extends readonly any[] = []
 > {
   new (
     settings: C,
-    value: T,
+    value?: T,
     ...args: G
   ): I;
 }
