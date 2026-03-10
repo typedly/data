69379e649375885b1a9b4cd0646756c475693e21
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-03-10 01:45:39 +0000

    refactor(`ConfigurableDataShape`): improve type imports and documentation clarity, use `Configurable`

--- src/configurable/lib/configurable.data.shape.ts
+++ src/configurable/lib/configurable.data.shape.ts
@@ -1,25 +1,19 @@
 // Type.
-import { DataConfig, InferAsync } from "../../lib/type";
+import type { InferAsync } from "../../lib/type";
 // Interface.
-import { DataSettings, DataShape } from "../../lib/interface";
+import type { DataSettings, DataShape } from "../../lib/interface";
+import type { Configurable } from "../../traits";
 /**
  * @description The shape of a data type with configuration.
  * @export
  * @interface ConfigurableDataShape
  * @template {DataSettings<R> | undefined} C The settings to configure the data shape.
- * @template T The value type.
+ * @template {T} [T=any] The value type.
  * @template {boolean} [R=InferAsync<C>] The async flag inferred from the settings.
- * @extends {DataShape<T, R>} 
+ * @extends {DataShape<T, R>} The base data shape with value type `T` and async flag `R`.
  */
 export interface ConfigurableDataShape<
   C extends DataSettings<R> | undefined,
-  T,
+  T = any,
   R extends boolean = InferAsync<C>
-> extends DataShape<T, R> {
-  /**
-   * @description The configuration of the `Data` instance.
-   * @readonly
-   * @type {?DataConfig<C, R>}
-   */
-  readonly configuration?: DataConfig<C, R>;
-}
+> extends DataShape<T, R>, Configurable<C, R> {}
