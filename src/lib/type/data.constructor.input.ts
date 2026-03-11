c1186863c043678f299f6df520e5db12a34b1a2f
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-03-11 01:31:48 +0000

    refactor: update import paths for InferAsync and InferValue in data constructor types and set default value to just empty array

--- src/lib/type/data.constructor.input.ts
+++ src/lib/type/data.constructor.input.ts
@@ -2,7 +2,7 @@
 import { DataConstructor, DataShape } from "..";
 // Type.
 import { DataConstructorTuple } from "./data.constructor.tuple";
-import { InferAsync } from "./infer-async.type";
+import { InferAsync } from "../../inference";
 /**
  * @description The input type for data constructors, with arguments support.
  * @export
@@ -10,11 +10,11 @@ import { InferAsync } from "./infer-async.type";
  * @template {{ async?: boolean }} [C=I extends DataShape<any, infer V, any> ? V : any] The type of the configuration, inferred from I if possible.
  * @template [T=I extends DataShape<infer U, any, any> ? U : any] The type of value instance, inferred from I if possible.
  * @template {boolean} [R=I extends DataShape<T, C, infer U> ? U extends boolean ? U : false : false] The type of the readonly flag, inferred from I if possible.
- * @template {readonly any[]} [G=any[]] The type of the arguments tuple.
+ * @template {readonly any[]} [G=[]] The type of the arguments tuple.
  */
 export type DataConstructorInput<
   I extends DataShape<T, R>,
   T = I extends DataShape<infer U, any> ? U : any,
   R extends boolean = InferAsync<I>,
-  G extends readonly any[] = any[]
+  G extends readonly any[] = []
 > = DataConstructor<I, T, R, G> | DataConstructorTuple<I, T, R, G>;
--- src/lib/type/data.constructor.tuple.ts
+++ src/lib/type/data.constructor.tuple.ts
@@ -1,19 +1,18 @@
 // Interface.
 import { DataShape, DataConstructor } from "..";
 // Type.
-import { InferAsync } from "./infer-async.type";
-import { InferValue } from "./infer-value.type";
+import { InferAsync, InferValue } from "../../inference";
 /**
  * @description The type to provide data constructor with arguments.
  * @export
  * @template {DataShape<T, R>} I The type of the data instance.
  * @template [T=InferValue<I>] The type of value instance, inferred from I if possible.
  * @template {boolean} [R=InferAsync<I>] The type of the readonly flag, inferred from I if possible.
- * @template {readonly any[]} [G=any[]] The type of the arguments tuple.
+ * @template {readonly any[]} [G=[]] The type of the arguments tuple.
  */
 export type DataConstructorTuple<
   I extends DataShape<T, R>,
   T = InferValue<I>,
   R extends boolean = InferAsync<I>,
-  G extends readonly any[] = any[]
+  G extends readonly any[] = []
 > = [DataConstructor<I, T, R, G>, ...G];
