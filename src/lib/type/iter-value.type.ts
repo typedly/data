a9fe022f704f7caf19d6141d078b226b5d1081c7
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-03-11 01:30:46 +0000

    chore(iterable): move to iterable

--- src/lib/type/iter-value.type.ts
+++
@@ -1,8 +0,0 @@
-// Type.
-import { IterableElement } from "./iterable-element.type";
-/**
- * @description The iterated value type.
- * @export
- * @template T The type of iterated value constrained by the `Iterable` type.
- */
-export type IterValue<T> = IterableElement<T>;
--- src/lib/type/iterable-element.type.ts
+++
@@ -1,6 +0,0 @@
-/**
- * @description The iterate element type.
- * @export
- * @template T The type of the iterable.
- */
-export type IterableElement<T> = T extends Iterable<infer U> ? U : T;
