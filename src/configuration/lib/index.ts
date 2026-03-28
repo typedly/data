d9121133fca2c7bc3c6d678ca7a504a61a55d918
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-03-28 11:59:08 +0000

    chore(api): export `CacheableDataSettings` and `CacheableSettings` types

--- src/configuration/lib/index.ts
+++ src/configuration/lib/index.ts
@@ -1,2 +1,4 @@
+export type { CacheableDataSettings } from './cacheable.data.settings';
+export type { CacheableSettings } from './cacheable.settings';
 export type { DataConfig } from './data.config';
 export type { DataSettings } from './data.settings';
